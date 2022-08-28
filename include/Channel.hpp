8c98e90c2f05c88cad009469e8c4674aa58176c8
parkjoungwan <whddhs6645@gmail.com>
2022-08-28 15:03:04 +0900

    Channel cpp&hpp

---
+++ include/Channel.hpp
@@ -0,0 +1,28 @@
+#ifndef	CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include <vector>
+#include <string>
+#include "Client.hpp"
+
+class Client;
+
+class Channel
+{
+	private:
+		int					_operator;
+		std::string			_channelName;
+		std::vector<int>	_myClientFdList;
+
+	public:
+		Channel(std::string	channelName, int fd);
+		~Channel();
+		bool	checkClientInChannel(int fd);
+		int		getMyOperator();
+		void	setMyOperator(int fd);
+		std::string			getChannelName();
+		std::vector<int>	getMyClientFdList();
+		void				addMyClientList(int fd);
+		void				removeClientList(int fd);
+		std::vecotr<int>::iterator	findMyClientIt(int fd);
+};
