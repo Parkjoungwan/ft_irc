e0486d6a9f3199558100eb95ad6ed9146fe5aff5
parkjoungwan <whddhs6645@gmail.com>
2022-08-30 16:07:44 +0900

    fin

---
+++ include/Server.hpp
@@ -0,0 +1,58 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <vector>
+#include <unistd.h>
+#include <map>
+#include <sstream>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/poll.h>
+#include <netinet/in.h>
+#include "Channel.hpp"
+#include "Client.hpp"
+#include "Command.hpp"
+#include "Util.hpp"
+
+class Channel;
+class Client;
+class Command;
+
+class Server
+{
+	private:
+		int	_port;
+		int	_serverSocketFd;
+		int _pollLet;
+		int	_maxClient;
+		int	_clientFd;
+		std::string _password;
+		socklen_t	_cliientLen;
+		sockaddr_in	_serverSocketaddr;
+		sockaddr_in	_clientAddr;
+		pollfd		_pollClient[OPEN_MAX];
+		std::map<std::string, Channel *>	_channelList;
+		std::map<int, Client *>				_ClientList;
+		Command								_command;
+
+		int		pollingEvent();
+		int		sock_init();
+		void	relayEvent();
+		void	check_cmd(std::vector<std::string> cmd_vec, Client *client);
+	public:
+		Server(int port, std::string password);
+		~Server();
+
+		std::map<int, Client *>				&getClientList();
+		std::map<std::string, Channel *>	&getChannelList();
+		std::string							getPass();
+		Client								*findClient(int fd);
+		Client								*findClient(std::string nick);
+		Channel								*findChannel(std::string name);
+		void								addChannelList(std::string channelName, int fd);
+		void								removeUnconnectClient(int fd);
+		int									execute();
+};
+
+#endif
---
+++ src/Server.cpp
@@ -0,0 +1,253 @@
+#include <fcntl.h>
+#include "../include/Server.hpp"
+
+Server::Server(int port, std::string password) : _command(this)
+{
+	_pollLet = 0;
+	_maxClient = 0;
+	_port = port;
+	_password = password;
+
+	//generate serversocket
+	sock_init();
+	_pollClient[0].fd = _serverSocketFd;
+	_pollClient[0].events = POLLIN;
+	for (int i = 1; i < OPEN_MAX; i++)
+		_pollClient[i].fd = -1;
+}
+
+int	Server::pollingEvent()
+{
+	_clientLen = sizeof(_clientAddr);
+	//generate socket for data recv
+	_clientFd = accept(_serverSocketFd, (struct sockaddr *)&_clientAddr, &_clientLen);
+	if (_clientFd < 0)
+	{
+		std::cerr << "Error: fail to accept client" << std::endl;
+		exit (1);
+	}
+	int fcntlRet = fcntl(_clientFd, F_SETFL, O_NONBLOCK);
+	if (fcntlRet == -1)
+	{
+		std::cerr << "Error: fail to fcntl" << std::endl;
+		exit (1);
+	}
+
+	_clientList.insert(std::pair<int, Client *>(_clientFd, new Client(_clientFd)));
+	std::cout << "Accept Client fd: " << _clientList.find(_clientFd)->first << "*" << std::endl;
+	
+	int index;
+	for (index = 1; index < OPEN_MAX; index++)
+	{
+		if (_pollClient[index].fd < 0)
+		{
+			_pollClient[index].fd = _clientFd;
+			break;
+		}
+	}
+	_pollClient[index].events = POLLIN;
+	if (index > _maxClient)
+		_maxClient = index;
+	if (--_pollLet <= 0)
+		return (-1);
+	return (0);
+}
+
+int	Server::sock_init()
+{
+	_serverSocketFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	_serverSocketAddr.sin_family = AF_INET;
+	_serverSocketAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	_serverSocketAddr.sin_port = htons(_port);
+	for (int i = 0; i < 8; i++)
+		_serverSocketAddr.sin_zero[i] = 0;
+
+	int			optval = true;
+	socklen_T	optlen = sizeof(optval);
+	setsockopt(_serverSocketFd, SOL_SOCKET, SO_REUSEADDR, (void *)optval, optlen);
+	
+	if (bind(_serverSocketFd, (const sockaddr *)&_serverSocketAddr, sizeof(_serverSocketAddr)) == -1)
+	{
+		std::cerr << "Error: fail to bind" << std::endl;
+		exit (1);
+	}
+	if (listen(_serverSocketFd, 15) == -1)
+	{
+		std::cerr << "Error: fail to listen" << std::endl;
+		exit (1);
+	}
+	std::cout << "listening" << std::endl;
+	return (0);
+}
+
+void	Server::relayEvent()
+{
+	char	buff[512];
+	for (int i = 1; i <= _maxClient; i++)
+	{
+		if (_pollClient[i].fd < 0)
+			continue;
+		if (_pollClient[i].revents & (POLLIN))
+		{
+			memset(buf, 0x00, 512);
+			//recv data
+			if (recv(_pollClient[i].fd, buf, 512, 0) == -1)
+			{
+				std::vector<std::string> tmp_vec;
+				tmp_vec.push_back("QUIT");
+				tmp_vec.push_back(":lost connection");
+				_command.quit(tmp_vec, findClient(_pollClient[i].fd));
+				_pollClient[i].fd = -1;
+			}
+			else
+			{
+				Client	*tmp = (_clientList.find(_pollClient[i].fd))->second;
+				tmp->appendRecvBuffer(std::string(buf));
+				
+				std::cout << "---- recvMsgBuf ----" << std::endl;
+				std::cout << "[" << tmp->getRecvBuffer() << "]" << std::endl;
+				std::cout << "pollfd: " << _pollClient[i].fd << std::endl;
+				std::cout << "--- endRecvMsgBuf ---" << std::endl;
+				
+				if (tmp->getRecvBuffer.find("\r\n") == std::string::npos)
+					continue;
+				std::vector<std::string> cmd = split(tmp->getRecvBuffer(), "\r\n");
+				if (cmd[0] == "")
+					continue;
+				print_stringVector(cmd);
+
+				if (!(tmp->getRegist() & REGI))
+					_command.welcome(cmd, (_clientList.find(_pollClient[i].fd))->second, _clientList);
+				else
+				{
+					std::vector<std::string>::iterator cmd_it = cmd.begin();
+					while (cmd_it != cmd.end())
+					{
+						std::vector<std::string> result = split(*cmd_it, " ");
+						check_cmd(result, tmp);
+						cmd_it++;
+					}
+					tmp->getRecvBuffer().clear();
+				}
+			}
+		}
+		else if (_pollClient[i].revents & POLLERR)
+		{
+			std::cerr << "Error: fail to poll" << std::endl;
+			exit(1);
+		}
+	}
+	std::map<int, Client *>::iterator it = _clientList.begin();
+	for (; it != _clientList.end(); it++)
+	{
+		if (it->second->getMsgBuffer().empty() == false)
+		{
+			std::string str = it->second->getMsgBuffer();
+			send(it->first, str.c_str(), str.length(), 0);
+			std::cout << "sendMsgs to <" << it->first << ">" << std::endl;
+			std::cout << str << std::endl;
+			str.clear();
+			it->second->clearMsgBuffer();
+		}
+	}
+}
+
+void	Server::check_cmd(std::vector<std::string> cmd_vec, Client *client)
+{
+	if (cmd_vec[0] == "NICK")
+		_command.nick(cmd_vec, client);
+	else if (cmd_vec[0] == "JOIN")
+		_command.join(cmd_vec, client);
+	else if (cmd_vec[0] == "KICK")
+		_command.kick(cmd_vec, client);
+	else if (cmd_vec[0] == "PRIVMSG")
+		_command.privmsg(cmd_vec, client);
+	else if (cmd_vec[0] == "NOTICE")
+		_command.notice(cmd_vec, client);
+	else if (cmd_vec[0] == "PING")
+		_command.pong(cmd_vec, client);
+	else if (cmd_vec[0] == "PART")
+		_command.part(cmd_vec, client);
+	else if (cmd_vec[0] == "QUIT")
+		_command.quit(cmd_vec, client);
+	else if (cmd_vec[0] == "PASS" || cmd_vec[0] == "USER")
+		_command.alreadyRegist(client);
+	else
+		std::cout << cmd_vec[0] << ": undefined cmd\n\n";
+}
+
+std::map<int, Client *>	&Server::getClientList()
+{
+	return _clientList;
+}
+
+std::map<std::string, Channel *> &Server::getChannelList()
+{
+	return _channelList;
+}
+
+std::string	Server::getPass()
+{
+	return _password;
+}
+
+Client	*Server::findClient(int fd)
+{
+	std::map<int, Client *>::iterator it;
+	if ((it = _clientList.find(fd)) != _clientList.end())
+		return (it->second);
+	return NULL;
+}
+
+Client	*Server::findClient(std::string nick)
+{
+	std::map<int, Client *>::iterator it = _clientList.begin();
+	for (; it != _clientList.end(); it++)
+	{
+		if (it->second->getNickName() == nick)
+			return (it->second);
+	}
+	return NULL;
+}
+
+void	Server::addChannelList(std::string channelName, int fd)
+{
+	_channelList.insert(std::pair<std::string, Channel *>(channelName, new Channel(channelName, fd)));
+}
+
+void	Server::removeUnconnectClient(int fd)
+{
+	Client	*tmp = findClient(fd);
+
+	std::string str = tmp->getMsgBuffer();
+	send(fd, str.c_str(), str.length(), 0);
+
+	std::cout << "remove client Msg to <" << fd << ">" << std::endl;
+	std::cout << str << std::endl;
+	str.clear();
+	tmp->clearMsgBuffer();
+	getClientList().erase(fd);
+	close(fd);
+	delete tmp;
+}
+
+int		Server::execute()
+{
+	while (1)
+	{
+		_pollLet = poll(_pollClient, _maxClient + 1, -1);
+		if (pollLet == 0 || _pollLet == -1)
+		{
+			std::cerr << "Error: fail to poll" << std::endl;
+			break ;
+		}
+		if (_pollClient[0].revents & POLLIN)
+		{
+			if (pollingEvent() == -1)
+				continue;
+		}
+		relayEvent();
+	}
+	close(_serverSocketFd);
+	return (0);
+}
